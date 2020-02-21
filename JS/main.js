a4e1bc8910b33dd2fe2c728585222ad91658fba6
Jonathan Mefford <Jonathanmefford@Breannes-MacBook-Pro.local>
2020-02-21 11:45:48 -0500

    add files

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+var time = setInterval(countdownTimer, 1000);
+
+function countdownTimer() {
+    var current = new Date();
+    var finish = new Date('November 10, 2020 12:00:00');
+    var remaining = finish.getTime() - current.getTime();
+
+    var d = Math.floor(remaining / (1000 * 60 * 60 * 24));
+    var h = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((remaining % (1000 * 60)) / 1000);
+
+    document.getElementById('countdown').innerHTML = d + ' days ' + h + ' hours ' + m + ' mins ' + s + ' secs ';
+
+    if (remaining <= 0) {
+        clearInterval(time);
+    }
+}
+countdownTimer();
